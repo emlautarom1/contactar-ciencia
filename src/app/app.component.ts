c5bc720e880cfc059f4eae8c938f88f317df645b
Lautaro Emanuel <emlautarom1@gmail.com>
2022-10-09 20:32:04 -0300

    Initial profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { MapGeocoder } from '@angular/google-maps';
-import { catchError, map, of, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tu-entrevistadx';
-  apiStatus$: any;
-
-  constructor(httpClient: HttpClient, private geocoder: MapGeocoder) {
-    const apiKey = "";
-    const url = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
-    this.apiStatus$ = httpClient.jsonp(url, 'callback')
-      .pipe(
-        map(() => true),
-        catchError((err) => {
-          console.log(err);
-          return of(false);
-        }),
-        shareReplay()
-      );
-  }
-}
+export class AppComponent { }
--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { MapGeocoder } from '@angular/google-maps';
+import { catchError, map, of, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
+  apiStatus$: any;
+  mapOptions: google.maps.MapOptions = {
+    disableDefaultUI: true
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(httpClient: HttpClient, private geocoder: MapGeocoder) {
+    const apiKey = "";
+    const url = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+    this.apiStatus$ = httpClient.jsonp(url, 'callback')
+      .pipe(
+        map(() => true),
+        catchError((err) => {
+          console.log(err);
+          return of(false);
+        }),
+        shareReplay()
+      );
   }
 
 }
