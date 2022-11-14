0e46ef23c33150eac35d56e9fa6a54f78ead26e9
Lautaro Emanuel <emlautarom1@gmail.com>
2022-11-14 15:56:38 -0300

    Clean up imports

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
 import { SessionService } from 'src/app/service/session.service';
 
 @Component({
