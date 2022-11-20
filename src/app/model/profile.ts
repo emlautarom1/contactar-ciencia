5f4009c81b016b95d771dc0d830133e0625a4bba
Lautaro Emanuel <emlautarom1@gmail.com>
2022-11-20 18:56:08 -0300

    Add 'profile' model

---
+++ src/app/model/profile.ts
@@ -0,0 +1,23 @@
+interface Profile {
+  name: string,
+  picture: string,
+  contact: {
+    phone: string,
+    email: string,
+    urls: string[]
+  },
+  location: {
+    name: string,
+    coordinates: { lat: number, lng: number }
+  },
+  cience: string,
+  specialization: string,
+  skills: string[],
+  cover: string,
+  work: {
+    title: string,
+    start_date: string,
+    end_date?: string,
+    description: string
+  }[]
+}
