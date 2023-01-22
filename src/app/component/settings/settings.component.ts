5e77ee751da7bf8029df8cd76cc1e969b4a65be1
Lautaro Emanuel <emlautarom1@gmail.com>
2023-01-21 21:17:58 -0300

    Added 'workForm'

--- src/app/component/settings/settings.component.ts
+++ src/app/component/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { SessionService } from 'src/app/service/session.service';
+import { ValuesService } from 'src/app/service/values.service';
 
 @Component({
   selector: 'app-settings',
@@ -9,14 +10,23 @@ import { SessionService } from 'src/app/service/session.service';
 })
 export class SettingsComponent implements OnInit {
   aboutForm: any;
+  picturePreview: any;
+
+  // TODO: Rewrite the science->specialization system
+  workForm: any;
+  workFormOptions: any;
 
   constructor(
     public session: SessionService,
     private fb: FormBuilder,
+    private values: ValuesService,
   ) { }
 
   ngOnInit(): void {
     let initial = this.session.profile;
+
+    this.workFormOptions = this.values.sciences;
+    this.picturePreview = initial.picture
 
     this.aboutForm = this.fb.group({
       name: [initial.name],
@@ -24,8 +34,13 @@ export class SettingsComponent implements OnInit {
       province: [initial.location.province],
       cover: [initial.cover],
       // TODO: Handle image upload
-      picture: [initial.picture],
+      picture: [null],
     });
 
+    this.workForm = this.fb.group({
+      science: [null],
+      specialization: [null],
+      skills: [""],
+    });
   }
 }
