e10676caf485a2ad3e372e0e9f919c9f9042616a
Lautaro Emanuel <emlautarom1@gmail.com>
2023-02-12 21:27:09 -0300

    Keep profile + auth in sync

--- src/app/service/session.service.ts
+++ src/app/service/session.service.ts
@@ -1,22 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Auth, authState, signInWithEmailAndPassword, signOut, User } from '@angular/fire/auth';
-import { collection, Firestore, getDocs, limit, query, where } from '@angular/fire/firestore';
-import { from, Observable, of, shareReplay, switchMap } from 'rxjs';
+import { collection, DocumentData, Firestore, getDocs, limit, onSnapshot, query, QueryDocumentSnapshot, QuerySnapshot, updateDoc, where } from '@angular/fire/firestore';
+import { map, Observable, of, OperatorFunction, shareReplay, switchMap, tap } from 'rxjs';
 import { Profile } from 'src/app/model/domain';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
-  currentUser$: Observable<User | null>
-  currentProfile$: Observable<Profile | null>
+  currentUser$!: Observable<User | null>
+  currentProfile$!: Observable<Profile | null>
 
-  constructor(private auth: Auth, private store: Firestore) {
+  constructor(private auth: Auth, private store: Firestore, private ngZone: NgZone) {
     this.currentUser$ = authState(this.auth);
     this.currentProfile$ = this.currentUser$.pipe(
-      switchMap(user => user ? from(this.fetchProfileByUserId(user.uid)) : of(null)),
-      shareReplay(1)
+      switchMap(user => user ? this.fetchProfileRef(user.uid) : of(null)),
+      map(snapshot => snapshot ? snapshot.data() as Profile : null),
+      shareReplay(1),
     );
+  }
+
+  private fetchProfileRef(uid: string): Observable<QueryDocumentSnapshot<DocumentData>> {
+    let c = collection(this.store, "profile");
+    let q = query(c, where("uid", "==", uid), limit(1));
+    return new Observable<QuerySnapshot<DocumentData>>(obs =>
+      onSnapshot(q, obs)).pipe(
+        runInZone(this.ngZone),
+        map(forEachToArray),
+        map(x => x[0]),
+      );
   }
 
   async logIn(email: string, password: string) {
@@ -27,12 +39,28 @@ export class SessionService {
     await signOut(this.auth);
   }
 
-  async fetchProfileByUserId(uid: string): Promise<Profile | null> {
+  async updateProfile(profile: Profile) {
     let profilesRef = collection(this.store, "profile");
-    let querySnapshot = await getDocs(query(profilesRef, where("uid", "==", uid), limit(1)));
-    let profiles: Profile[] = [];
-    querySnapshot.forEach(doc => profiles.push(doc.data() as Profile));
+    let querySnapshot = await getDocs(query(profilesRef, where("uid", "==", profile.uid), limit(1)));
+    let queryDocumentSnapshot = forEachToArray(querySnapshot);
 
-    return profiles[0];
+    updateDoc(queryDocumentSnapshot[0].ref, { ...profile });
   }
+}
+
+function forEachToArray<T>(arg: { forEach: (callback: (e: T) => void) => void }): T[] {
+  let elements: T[] = []
+  arg.forEach(e => { elements.push(e); });
+  return elements;
+}
+
+export function runInZone<T>(zone: NgZone): OperatorFunction<T, T> {
+  return (source) => {
+    return new Observable(observer => {
+      const next = (value: T) => zone.run(() => observer.next(value));
+      const error = (e: any) => zone.run(() => observer.error(e));
+      const complete = () => zone.run(() => observer.complete());
+      return source.subscribe({ next, error, complete });
+    });
+  };
 }
