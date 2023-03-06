8ca7b611b7e1bba6965c8880ac135a3a09044132
Lautaro Emanuel <emlautarom1@gmail.com>
2023-03-06 19:33:32 -0300

    Extract generic utils functions

---
+++ src/app/utils.ts
@@ -0,0 +1,19 @@
+import { NgZone } from "@angular/core";
+import { Observable, OperatorFunction } from "rxjs";
+
+export function forEachToArray<T>(arg: { forEach: (callback: (e: T) => void) => void }): T[] {
+  let elements: T[] = []
+  arg.forEach(e => { elements.push(e); });
+  return elements;
+}
+
+export function runInZone<T>(zone: NgZone): OperatorFunction<T, T> {
+  return (source) => {
+    return new Observable(observer => {
+      const next = (value: T) => zone.run(() => observer.next(value));
+      const error = (e: any) => zone.run(() => observer.error(e));
+      const complete = () => zone.run(() => observer.complete());
+      return source.subscribe({ next, error, complete });
+    });
+  };
+}
