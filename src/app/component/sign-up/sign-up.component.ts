ddbd13a4c95a0f1c4fb36a6b3555b98ec2bb57ce
Lautaro Emanuel <emlautarom1@gmail.com>
2023-03-06 21:10:16 -0300

    Create new users

--- src/app/component/sign-up/sign-up.component.ts
+++ src/app/component/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ValidationErrors, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProfileService } from 'src/app/service/profile.service';
+import { SessionService } from 'src/app/service/session.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  signupForm = this.fb.nonNullable.group({
+    name: ["", [Validators.required, Validators.minLength(8)]],
+    phoneNumber: ["", [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/[\d\+\-\s_()]+/)
+    ]],
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+  });
 
-  constructor() { }
+  signUpError: string | null = null;
+  isLoading: boolean = false;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private ps: ProfileService,
+    private session: SessionService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() { }
+
+  async onSubmit() {
+    if (this.signupForm.invalid) { return }
+
+    this.isLoading = true;
+    try {
+      let { name, phoneNumber, email, password } = this.signupForm.getRawValue();
+      await this.ps.createProfile(name, phoneNumber, email, password);
+      await this.session.logIn(email, password);
+      this.router.navigate(["/"]);
+    } catch (error) {
+      this.signUpError = (error as Error).message;
+    } finally {
+      this.isLoading = false;
+    }
   }
-
 }
--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { collection, Firestore, getDocs, limit, query, updateDoc, where } from '@angular/fire/firestore';
+import { Auth, AuthErrorCodes, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { addDoc, collection, Firestore, getDocs, limit, query, updateDoc, where } from '@angular/fire/firestore';
 import { Profile } from 'src/app/model/domain';
 import { forEachToArray } from 'src/app/utils';
 
@@ -9,14 +10,47 @@ import { forEachToArray } from 'src/app/utils';
 export class ProfileService {
 
   constructor(
+    private auth: Auth,
     private store: Firestore
   ) { }
 
+  async createProfile(name: string, phoneNumber: string, email: string, password: string) {
+    try {
+      let { user: { uid } } = await createUserWithEmailAndPassword(this.auth, email, password);
+      let initialProfile = this.buildInitialProfile(uid, name, phoneNumber, email);
+
+      await addDoc(collection(this.store, "profile"), initialProfile);
+    } catch (error: any) {
+      switch (error?.code) {
+        case AuthErrorCodes.EMAIL_EXISTS: throw new Error("El email ya está en uso");
+        case AuthErrorCodes.WEAK_PASSWORD: throw new Error("Contraseña muy débil");
+        default: throw new Error("Error al registrarse");
+      }
+    };
+  }
+
   async updateProfile(profile: Profile) {
-    let profilesRef = collection(this.store, "profile");
-    let querySnapshot = await getDocs(query(profilesRef, where("uid", "==", profile.uid), limit(1)));
+    let querySnapshot = await getDocs(
+      query(collection(this.store, "profile"),
+        where("uid", "==", profile.uid), limit(1))
+    );
     let queryDocumentSnapshot = forEachToArray(querySnapshot);
 
     await updateDoc(queryDocumentSnapshot[0].ref, { ...profile });
+  }
+
+  private buildInitialProfile(uid: string, name: string, phoneNumber: string, email: string): Profile {
+    return {
+      uid,
+      name,
+      pictureURL: "assets/avatar/placeholder.png",
+      contact: { phone: phoneNumber, email, urls: [] },
+      location: { city: "", province: "" },
+      science: "",
+      specialization: "",
+      skills: [],
+      coverLetter: "",
+      workExperience: []
+    }
   }
 }
