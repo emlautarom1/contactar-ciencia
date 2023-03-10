93fc20118f9d39ff1e50553eb56550c1040e7d0c
Lautaro Emanuel <emlautarom1@gmail.com>
2023-03-09 22:10:29 -0300

    Initial search logic

--- src/app/component/search/search.component.ts
+++ src/app/component/search/search.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map, shareReplay, switchMap, tap } from 'rxjs';
+import { SearchService } from 'src/app/service/search.service';
 import { ValuesService } from 'src/app/service/values.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { ValuesService } from 'src/app/service/values.service';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
   searchForm = this.fb.group({
     term: [""],
     science: [null as string | null],
@@ -21,14 +22,37 @@ export class SearchComponent implements OnInit {
     map(science => science ? this.values.specializationsFor(science) : [])
   );
 
+  searchTerms$ = this.route.queryParamMap.pipe(
+    map(params => this.mergeParamMap(params, this.searchForm.getRawValue())),
+    shareReplay(1)
+  );
+  searchResults$ = this.searchTerms$.pipe(
+    switchMap(terms => this.search.byTerms(terms))
+  );
+
+  searchTermsSyncSub = this.searchTerms$.subscribe(
+    terms => this.searchForm.patchValue(terms)
+  );
+
   constructor(
     private route: ActivatedRoute,
+    private search: SearchService,
     private values: ValuesService,
     private fb: FormBuilder,
   ) { }
 
-  ngOnInit(): void {
-    this.route.queryParamMap.subscribe(console.log)
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {
+    this.searchTermsSyncSub.unsubscribe();
+  }
+
+  mergeParamMap<T extends Record<string, any>>(paramMap: ParamMap, currentValue: T) {
+    let merged: { [key: string]: any } = {};
+    for (let k of Object.keys(currentValue)) {
+      merged[k] = paramMap.get(k) || currentValue[k]
+    }
+    return merged;
   }
 
 }
--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -4,6 +4,13 @@ import { map, Observable, shareReplay } from 'rxjs';
 import { Profile } from '../model/domain';
 import { forEachToArray, groupByKey, runInZone } from '../utils';
 
+export interface SearchTerms {
+  term: string,
+  science: string | null,
+  specialization: string | null,
+  location: string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -21,6 +28,29 @@ export class SearchService {
     private store: Firestore,
     private ngZone: NgZone,
   ) { }
+
+  async byTerms(terms: Partial<SearchTerms>): Promise<Profile[]> {
+    let placeholder: Profile = {
+      uid: "IAi43S5PogNKuCDYWGqfsYxXi273",
+      name: "Patricia Estela Verdes",
+      pictureURL: "https://firebasestorage.googleapis.com/v0/b/tu-entrevistadx.appspot.com/o/IAi43S5PogNKuCDYWGqfsYxXi273.jpeg?alt=media&token=c9b9139a-55ec-494f-88c7-8d801ed59984",
+      contact: {
+        phone: "266-404-2222",
+        email: "peverdes@unsl.edu.ar",
+        urls: ["http://twitter.com/peverdes"]
+      },
+      location: {
+        city: "Merlo",
+        province: "San Luis"
+      },
+      science: "Agrarias, Ingenierías, Desarrollo Tecnológico y Social",
+      specialization: "Ciencias Agrarias",
+      skills: ["Alimentos", "Salud", "Química", "Ambiente"],
+      coverLetter: "El potencial productivo de los recursos vegetales nativos de la provincia de San Luis es valioso, ya sea por su potencial forrajero, ornamental, medicinal, industrial y/o alimenticio. El aumento de la población, la necesidad de mejorar la calidad de dieta de los habitantes y los crecientes requerimientos de biocombustibles y biomateriales, entre otros aspectos, llevan a una mayor demanda de productos agropecuarios.",
+      workExperience: Array(5).fill({ title: "Mi Proyecto", start_date: "01/2022", end_date: "08/2022", description: "Herramientas biotecnológicas para la domesticación, caracterización y desarrollo de germoplasma nativo con potencial ornamental, aromático y medicinal de la provincia de San Luis" })
+    }
+    return Array(5).fill(placeholder);
+  }
 
   private allUsers(): Observable<QueryDocumentSnapshot<DocumentData>[]> {
     let allUsers = query(collection(this.store, "profile"));
